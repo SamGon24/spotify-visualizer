d8dc7ca456afbf74658699b9f9591645fe14d599
Samuel Gonzalez Pineda <vzo20@txstate.edu>
2025-10-08 16:09:33 -0500

    Establishing the start of the pipeline
    Adding the starting point of the project

--- retrieve.py
+++ retrieve.py
@@ -1,1 +1,3 @@
-# First file. Initial commit.
+# This file is the first step in the data pipeline.
+# It retrieves the user's recently played tracks from the Spotify API
+# and saves the data to a CSV file for further processing.
