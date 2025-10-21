b782c3ed486cd03025beaa5cebecc6ecaf0bede4
Samuel Gonzalez Pineda <vzo20@txstate.edu>
2025-10-21 14:02:52 -0500

    creating retrieve.py
    to gather info from spotify

--- retrieve.py
+++ retrieve.py
@@ -1,3 +1,29 @@
 # This file is the first step in the data pipeline.
 # It retrieves the user's recently played tracks from the Spotify API
 # and saves the data to a CSV file for further processing.
+
+from __future__ import annotations
+import os
+import pandas as pd
+import json
+import sys
+
+from dotenv import load_dotenv
+
+def _require_env(var: str) -> str:
+    value = os.getenv(var)
+    if value is None:
+        raise EnvironmentError(f"Required environment variable '{var}' not found.")
+    return value
+
+def _get_spotify_client():
+    try:
+        import spotipy
+        from spotipy.oauth2 import SpotifyOAuth
+    except ImportError:
+        raise ImportError("The 'spotipy' library is required to run this script. Please install it via 'pip install spotipy'.",
+                          file=sys.stderr)
+
+    # justto check everything is working (dependencies)
+
+
