8506298b1e349c3f0983917051c4d91a3099c22d
Samuel Gonzalez Pineda <vzo20@txstate.edu>
2025-12-27 23:29:08 -0600

    Refactored everything into app

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,17 @@
-# app.py
-import os, sys
-sys.path.append(os.path.dirname(__file__))  # fuerza a Python a usar tu retrieve.py local
-
-from flask import Flask, jsonify
+from flask import Blueprint, jsonify
 from retrieve import _get_spotify_client, get_recently_played
 
-app = Flask(__name__)
+api_bp = Blueprint("api", __name__)
 
-@app.route("/")
+@api_bp.route("/")
 def home():
     return "<h1>Spotify Visualizer</h1><p>Visit /user or /recent to test the API.</p>"
 
-@app.route("/user")
+@api_bp.route("/user")
 def user_info():
     sp = _get_spotify_client()
     me = sp.current_user()
+
     user_data = {
         "display_name": me.get("display_name"),
         "id": me.get("id"),
@@ -23,11 +20,8 @@ def user_info():
     }
     return jsonify(user_data)
 
-@app.route("/recent")
+@api_bp.route("/recent")
 def recent_tracks():
     sp = _get_spotify_client()
     tracks = get_recently_played(sp, limit=10)
     return jsonify(tracks)
-
-if __name__ == "__main__":
-    app.run(debug=True)
